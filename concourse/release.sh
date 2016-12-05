f5ede79a8c9a6d7d56c56de327c160594458d5f5
Ivan Borshukov <bo0merzzz@gmail.com>
2016-12-05 11:40:04 +0200

    I'm stupid

--- concourse/release.sh
+++ concourse/release.sh
@@ -9,6 +9,14 @@ git log `git describe --tags --abbrev=0`..HEAD --oneline > release_body
 cat release_body
 
 mkdir -p build/
-GOOS=linux GOARCH=amd64 go build -v -o build/dp_linux_amd64
-GOOS=darwin GOARCH=amd64 go build -v -o build/dp_darwin_amd64
-GOOS=windows GOARCH=amd64 go build -v -o build/dp_windows_amd64.exe
+build_dir=$(cd build && pwd)
+
+project_path=$GOPATH/src/github.com/Bo0mer/
+mkdir -p $project_path
+cp -r dp $project_path
+
+cd $project_path
+
+GOOS=linux GOARCH=amd64 go build -v -o $build_dir/dp_linux_amd64
+GOOS=darwin GOARCH=amd64 go build -v -o $build_dir/dp_darwin_amd64
+GOOS=windows GOARCH=amd64 go build -v -o $build_dir/dp_windows_amd64.exe
