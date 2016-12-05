b144ced5dddd31da9296f159d78e4debde2f7956
Ivan Borshukov <bo0merzzz@gmail.com>
2016-12-05 11:24:57 +0200

    Build for linux, dawrin and windows

--- concourse/release.sh
+++ concourse/release.sh
@@ -1,4 +1,4 @@
-set -e
+set -ex
 
 cd dp
 version=$(cat .version)
@@ -6,3 +6,9 @@ echo -e "v$version" > release_name
 echo -e "v$version" > release_tag
 
 git log `git describe --tags --abbrev=0`..HEAD --oneline > release_body
+cat $release_body
+
+mkdir -p build/
+GOOS=linux GOARCH=amd64 go build -o build/dp_linux_amd64
+GOOS=darwin GOARCH=amd64 go build -o build/dp_darwin_amd64
+GOOS=windows GOARCH=amd64 go build -o build/dp_windows_amd64.exe
