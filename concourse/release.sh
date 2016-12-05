a27af7c37d2fb4f3e18e0051aee14dab3b09f0db
Ivan Borshukov <bo0merzzz@gmail.com>
2016-12-05 11:35:40 +0200

    Build with verbose flag

--- concourse/release.sh
+++ concourse/release.sh
@@ -9,6 +9,6 @@ git log `git describe --tags --abbrev=0`..HEAD --oneline > release_body
 cat release_body
 
 mkdir -p build/
-GOOS=linux GOARCH=amd64 go build -o build/dp_linux_amd64
-GOOS=darwin GOARCH=amd64 go build -o build/dp_darwin_amd64
-GOOS=windows GOARCH=amd64 go build -o build/dp_windows_amd64.exe
+GOOS=linux GOARCH=amd64 go build -v -o build/dp_linux_amd64
+GOOS=darwin GOARCH=amd64 go build -v -o build/dp_darwin_amd64
+GOOS=windows GOARCH=amd64 go build -v -o build/dp_windows_amd64.exe
